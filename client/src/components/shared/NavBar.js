14b4c8b804d12102ae2769dfd9016c6d0b64e4f2
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-25 21:08:16 -0600

    Routers, home, about, navbar

---
+++ client/src/components/shared/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => (
+  <Menu>
+    <Link to="/">
+      <Menu.Item>
+        Home
+      </Menu.Item>
+    </Link> 
+    <Link to="/about">
+      <Menu.Item>
+        About
+      </Menu.Item>
+    </Link>  
+    <Link to="/handymens">
+      <Menu.Item>
+        Handy Man
+      </Menu.Item>
+    </Link>  
+  </Menu>
+)
+
+export default NavBar
