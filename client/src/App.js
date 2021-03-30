e46110341fbcf11f78c0f7e114dbf4830c2a2f8a
Jayden <jaydenowens23@gmail.comgit config --global core.editor vimtouch ~/.gitignorecode ~/.gitignoregit config --global core.excludesfile ~/.gitignore>
2021-03-30 08:41:23 -0600

    services forum

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import About from './components/shared/About';
 import NoMatch from './components/shared/NoMatch';
 import NavBar from './components/shared/NavBar';
 import HandyMens from './components/handymens/Handymens'
+import Handymen from './components/handymens/Handymen';
 
 const App = () => (
   <>
@@ -13,6 +14,7 @@ const App = () => (
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
         <Route exact path="/handymens" component={HandyMens}/>
+        <Route exact path="/handymens/:id" component={Handymen} />
         <Route component={NoMatch}/>
       </Switch>
   </>
